7f4725b30a3e905ca0d3a0fc8f63e2572cd28535
bfrpaulondev <bfrpaulondev@gmail.com>
2025-01-30 15:15:57 +0000

    add allowed formats for file uploads in multer configuration

--- src/config/multer.js
+++ src/config/multer.js
@@ -6,7 +6,27 @@ const cloudinary = require('./cloudinary');
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'gerenciador_de_tarefas', // Nome da pasta no Cloudinary
+    folder: 'gerenciador_de_tarefas',
+    allowed_formats: [
+      'jpg', 'png', 'jpeg',     // Imagens
+      'pdf',                    // Documentos PDF
+      'csv',                    // Planilhas CSV
+      'doc', 'docx',            // Documentos Word
+      'xls', 'xlsx',            // Planilhas Excel
+      'ppt', 'pptx',            // Apresentações PowerPoint
+      'txt',                    // Arquivos de texto
+      'gif', 'webp',            // Outros formatos de imagem
+      'zip',                    // Arquivos compactados
+      'odt'                   ,
+      'ods'                   ,
+      'odp'                   ,
+      'mp3', 'wav', 'ogg', 'flac', // Arquivos de áudio
+      'mp4', 'avi', 'mkv', 'webm', // Arquivos de vídeo
+      'csv',                    // Planilhas CSV
+      'txt',                    // Arquivos de texto
+      'gif', 'webp',            // Outros formatos de imagem
+
+    ]
   },
 });
 
